7705ddde6917a7a9eed3d3b512b72967deed752c
CGUERRA96 <christian.guerra101013@gmail.com>
2020-10-04 04:55:54 -0500

    Actualizacion v1

--- classes/venta.py
+++ classes/venta.py
@@ -1,1 +1,23 @@
 from connection.conn import Conexion
+
+class Venta:
+    def __init__(self):
+        self.model = Conexion('venta')
+
+    def guardar_venta(self, venta):
+        return self.model.insert(venta)
+
+    def obtener_venta(self, id_venta):
+        return self.model.get_by_id(id_venta)
+
+    def obtener_ventas(self, order):
+        return self.model.get_all(order)
+
+    def buscar_ventas(self, data_venta):
+        return self.model.get_by_column(data_venta)
+
+    def modificar_venta(self, id_venta, data_venta):
+        return self.model.update(id_venta, data_venta)
+
+    def eliminar_venta(self, id_venta):
+        return self.model.delete(id_venta)
--- classes/venta_detalle.py
+++ classes/venta_detalle.py
@@ -1,1 +1,23 @@
 from connection.conn import Conexion
+
+class Venta_detalle:
+    def __init__(self):
+        self.model = Conexion('venta_detalle')
+
+    def guardar_venta_detalle(self, venta_detalle):
+        return self.model.insert(venta_detalle)
+
+    def obtener_venta_detalle(self, id_venta_detalle):
+        return self.model.get_by_id(id_venta_detalle)
+
+    def obtener_ventas_detalles(self, order):
+        return self.model.get_all(order)
+
+    def buscar_ventas_detalles(self, data_venta_detalle):
+        return self.model.get_by_column(data_venta_detalle)
+
+    def modificar_ventas_detalles(self, id_venta_detalle, data_venta_detalle):
+        return self.model.update(id_venta_detalle, data_venta_detalle)
+
+    def eliminar_ventas_detalles(self, id_venta_detalle):
+        return self.model.delete(id_venta_detalle)
