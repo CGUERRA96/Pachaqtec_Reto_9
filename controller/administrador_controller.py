bd38b0e581c7269832b1baa3874ce8677c538836
Abraham Leiva <abraham.leiva@gmail.com>
2020-10-06 20:27:14 -0500

    Controlador administrador_controller en implementación

--- controller/administrador_controller.py
+++ controller/administrador_controller.py
@@ -1,10 +1,16 @@
-from classes.persona import Persona
 from helpers.helper import input_data, print_table, pregunta
 from helpers.menu import Menu
+from classes.venta import Venta
+from classes.producto import Producto
+from classes.venta_detalle import Venta_detalle
+from classes.persona import Persona
 
 class Administrador_controller:
     def __init__(self):
         self.persona = Persona()
+        self.producto = Producto()
+        self.venta = Venta()
+        self.venta_detalle = Venta_detalle()
         self.salir = False
 
     def menu(self):
@@ -15,11 +21,11 @@ class Administrador_controller:
                     Interfaz Administrador
                 ==============================
                 ''')
-                menu = ['Ingresar productos', 'Mantenimiento productos', "Salir"]
+                menu = ['Ver productos en almacen', 'Ver reporte de ventas', "Salir"]
                 respuesta = Menu(menu).show()
                 
                 if respuesta == 1:
-                    pass
+                    self.listar_productos()
                 elif respuesta == 2:
                     pass                
                 else:
@@ -27,3 +33,16 @@ class Administrador_controller:
                     break
             except Exception as e:
                 print(f'{str(e)}')
+
+        def listar_productos(self):
+            print('''
+            ==========================
+                Lista de Productos
+            ==========================
+            ''')
+            productos = self.producto.obtener_productos('id_productos')
+            print(print_table(productos, ['ID', 'Nombre Producto', 'ID Categoria', 'Fecha Ingreso', 'Vida Util', 'Valor Unitario Compra', 'Valor Unitario Venta', 'Exogeración', 'Stock']))
+            input("\nPresione una tecla para continuar...")
+
+        def reporte_ventas(self):
+            pass
