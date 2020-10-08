bdd8379f89d0434c22e720f11e1240659b09c56f
Abraham Leiva <abraham.leiva@gmail.com>
2020-10-07 19:45:27 -0500

    administador_controller modificado: se agrego reporte de ventas

--- controller/administrador_controller.py
+++ controller/administrador_controller.py
@@ -27,22 +27,47 @@ class Administrador_controller:
                 if respuesta == 1:
                     self.listar_productos()
                 elif respuesta == 2:
-                    pass                
+                    self.reporte_ventas()             
                 else:
                     self.salir = True
                     break
             except Exception as e:
                 print(f'{str(e)}')
 
-        def listar_productos(self):
-            print('''
-            ==========================
-                Lista de Productos
-            ==========================
-            ''')
-            productos = self.producto.obtener_productos('id_productos')
-            print(print_table(productos, ['ID', 'Nombre Producto', 'ID Categoria', 'Fecha Ingreso', 'Vida Util', 'Valor Unitario Compra', 'Valor Unitario Venta', 'Exogeración', 'Stock']))
-            input("\nPresione una tecla para continuar...")
+    def listar_productos(self):
+        print('''
+        ==========================
+            Lista de Productos
+        ==========================
+        ''')
+        productos = self.producto.obtener_productos('id_productos')
+        print(print_table(productos, ['ID', 'Nombre Producto', 'ID Categoria', 'Fecha Ingreso', 'Vida Util', 'Valor Unitario Compra', 'Valor Unitario Venta', 'Exogeración', 'Stock']))
+        input("\nPresione una tecla para continuar...")
 
-        def reporte_ventas(self):
-            pass
+    def reporte_ventas(self):
+        print('''
+        ========================
+            Reporte de Ventas
+        ========================
+        ''')
+        ventas_detalles = self.venta_detalle.obtener_ventas_detalles('id_venta_det')
+        lista_ventas = []
+        if ventas_detalles:
+            for venta in ventas_detalles:
+                buscar_venta = self.venta.obtener_venta({'id_venta': venta[1]})
+                buscar_producto = self.producto.obtener_producto({'id_producto': venta[2]})
+                cantidad = venta[3]
+                monto_sin_igv = venta[4]
+                igv = venta[5]
+                precio_venta = precio_venta[6]
+                lista_ventas.append((
+                    venta[0],
+                    buscar_venta[2],
+                    buscar_producto[1],
+                    cantidad,
+                    monto_sin_igv,
+                    igv,
+                    precio_venta
+                ))
+        print(print_table(lista_ventas, ['ID', 'Doc. Comprador', 'Producto', 'Cantidad', 'Precio sin IGV', 'IGV', 'Precio Final']))
+        input("\nPresione una tecla para continuar...")
